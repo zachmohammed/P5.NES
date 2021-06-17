2e24ffdc1cbc5a629c365f3df90b245fc7c9d000
Zach Mohammed <zach.mohammed@ada.ac.uk>
2021-06-17 15:49:22 +0100

    Playing custom roms

--- test.js
+++ test.js
@@ -7,10 +7,25 @@ function preload(){
 function setup() {
     createCanvas(256, 240);
 
-    romfile = LoadNESFile(rom)
+ 
+}
+function get_rom(){
+    var fileList = document.getElementById("rom").files;
+    var fileReader = new FileReader();
+    if (fileReader && fileList && fileList.length) {
+        url = URL.createObjectURL(fileList[0])
+        return loadBytes(url)
+    }
+}
+function loadarom(){
     
+    object = get_rom()
+    
+    print((object.bytes[1]))
+    romfile = LoadNESFile(object)
+    print(romfile)
     ram = []
-    mapper = new mapperobject1(romfile);
+    mapper = new mapperobject1(rom);
 
     
     ppu = new NESPPU(null, null, new paletteTable())
@@ -20,9 +35,7 @@ function setup() {
     ppu.mapper = new ppumemory(mapper, romfile, ppu)
     cpu.reset()
     ppu.reset()
-
 }
-
 function step(ppu, cpu){
     cpuCycles = this.cpu.step()
     ppuCycles = cpuCycles * 3
@@ -35,18 +48,19 @@ function step(ppu, cpu){
 }
   
 function draw() {
-
-    step(ppu,cpu)
     background(240);
-
-    for(i = 0; i < ppu.back.length; i++){
-        noStroke()
-        fill("#" + ppu.back[i][2])
-        rect(ppu.back[i][0],ppu.back[i][1],1,1)
+    if(romfile == null){
         
     }
-
+    else{
+        step(ppu,cpu)
+        
     
-    
-    
+        for(i = 0; i < ppu.back.length; i++){
+            noStroke()
+            fill("#" + ppu.back[i][2])
+            rect(ppu.back[i][0],ppu.back[i][1],1,1)
+            
+        }
+    }  
 }
